58a56dfd2a1fcf2510b1c1a6471af91a85da84d2
ASHISH PATEL <ASHISHPATELUK@GMAIL.COM>
2023-10-30 17:07:37 +0000

    P4 Commit

---
+++ MyClass4.java
@@ -0,0 +1,13 @@
+package mypackage4;
+
+
+
+public class MyClass4
+{
+    public static void main(String[]args)
+
+    {
+        System.out.println("What are Principles Of Software Testing?");
+    }
+
+}
